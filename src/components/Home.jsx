668656dd56b487ccb2ed15914d0a348e208df859
code-looney <danielvbofficieel@gmail.com>
2024-07-13 15:09:21 +0200

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+
+const Home = () => {
+  return (
+    <div className='bg-parallax h-screen w-full bg-no-repeat bg-cover outline'>
+        <Header />
+    </div>
+  )
+}
+
+export default Home
