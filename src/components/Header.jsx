da2182a358109e21120d60226a4211fc0542391a
code-looney <danielvbofficieel@gmail.com>
2024-07-21 14:01:37 +0200

    storing nav link snipped

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,14 @@ import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
-  const [hoverUnderlineColor, setHoverUnderlineColor] = useState("");
+  const [hoverUnderlineColor, setHoverUnderlineColor] = useState("transparent");
   const hoverState = ["border-b-4"]
 
-  function handleNavLinkHover () {
-    if (hoverUnderlineColor === "") {
-      setHoverUnderlineColor("border-b-4")
-    }
-  }
+  // function handleNavLinkHover (id) {
+  //   if (id === 2) {
+  //     setHoverUnderlineColor('white')
+  //   }
+  // }
 
 
   return (
@@ -17,27 +17,43 @@ const Header = () => {
         <div className='h-[40px] w-[187.5px] lg:h-[48px] lg:w-[160px] pl-[1.5em] lg:pl-0 lg:justify-center flex'>
             <img className='h-[40px] w-[40px] lg:w-[48px] lg:h-[48px] cursor-pointer' src="/assets/shared/logo.svg" alt="" />
         </div>
-        <div className=' h-[40px] w-[187.5px] lg:w-[640px] lg:h-[96px] flex justify-end items-center  lg:absolute right-0 top-0 lg:backdrop-blur-xl lg:bg-white/5 lg:pr-0 pr-[1.5em] '>
+        <div className=' h-[40px] w-[187.5px] lg:w-[640px] lg:h-[96px] flex justify-end items-center lg:absolute right-0 top-0 lg:backdrop-blur-xl lg:bg-white/5 lg:pr-0 pr-[1.5em] '>
             <Link><img className='h-[21px] w-[24px] lg:hidden' src="/assets/shared/icon-hamburger.svg" alt="" /></Link>
         </div>
-            <ul className='h-full w-full flex-row justify-end items-center pr-[3em] gap-[3em] text-[1rem] outline  lg:flex relative z-50 text-white uppercase'>
-                <li className={`flex h-full items-center ${hoverUnderlineColor} border-b-white  relative z-20`}>
-                  <span className='relative z-40'></span>
-                  <Link onTouchStart={handleNavLinkHover} className=' cursor-pointer'>Home</Link>
+            {/* <ul className='h-full w-full flex-row justify-end items-center pr-[3em] hidden gap-[3em] text-[1rem]  lg:flex relative z-50 text-white uppercase'>
+                <li className={`flex h-full items-center flex-col  justify-center  border-b-white  relative z-20`}>
+                  <Link
+                  id='1' 
+                  onTouchStart={() => handleNavLinkHover(id)}
+                  onTouchEnd={() => setHoverUnderlineColor("transparent")}
+                  className={`border-b-${hoverUnderlineColor} border-b-4 pt-[4px] h-full select-none flex items-center`}>Home</Link>
                 </li>
-                <li className='flex gap-[0.5em] h-full items-center border-b-4 border-b-white  relative z-20'>
+                
+                <li className='flex gap-[0.5em] h-full items-center relative z-20'>
                   <span className='relative z-40 '>01</span>
-                  <Link className=' cursor-pointer'>Destination</Link>
+                  <Link
+                    id='2'
+                    onTouchStart={() => handleNavLinkHover(id)}
+                    onTouchEnd={() => setHoverUnderlineColor("transparent")} 
+                    className={` cursor-pointer border-b-${hoverUnderlineColor} border-b-4 pt-[4px] h-full select-none flex items-center`}>Destination</Link>
                   </li>
-                <li className='flex gap-[0.5em] h-full items-center border-b-4 border-b-white  relative z-20'>
+                <li className='flex gap-[0.5em] h-full items-center relative z-20'>
                   <span className='relative z-40 '>02</span>
-                  <Link className=' cursor-pointer'>Crew</Link>
+                  <Link
+                    id='3'
+                    onTouchStart={() => setHoverUnderlineColor("white")}
+                    onTouchEnd={() => setHoverUnderlineColor("transparent")} 
+                    className=' cursor-pointer'>Crew</Link>
                   </li>
-                <li className='flex gap-[0.5em] h-full items-center border-b-4 border-b-white  relative z-20'>
+                <li className='flex gap-[0.5em] h-full items-center relative z-20'>
                   <span className='relative z-40 '>03</span>
-                  <Link className=' cursor-pointer'>Technology</Link>
+                  <Link
+                    id='4'
+                    onTouchStart={() => setHoverUnderlineColor("white")}
+                    onTouchEnd={() => setHoverUnderlineColor("transparent")} 
+                    className=' cursor-pointer'>Technology</Link>
                   </li>
-              </ul>
+              </ul> */}
     </div>
   )
 }
