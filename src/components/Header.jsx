8b6e0a33fbd235ff089e4f6edffe0f116bb31e8f
code-looney <danielvbofficieel@gmail.com>
2024-07-26 22:23:15 +0200

    desk

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,25 +15,26 @@ const Header = () => {
     .then(data => {
       setRouter(data.navigation)
       console.log(data.navigation[0].order)
+      console.log(data.navigation)
       setPageOrder(data.navigation.order)
     }
     )
   }, [])
 
   function handleNavLinkHover (id)  {
+    console.log(id)
     setHoverUnderlineColor(id)
   } 
 
-
   return (
-    <div className='w-full h-[88px] lg:h-[96px] flex justify-between items-center font-barlowCondensed tracking-widest'>
-        <div className='h-[px] w-[187.5px] lg:h-[48px] lg:w-[170px] pl-[1.5em] lg:pl-0 lg:justify-center flex'>
-            <img className='h-[40px] w-[40px] lg:w-[48px] lg:h-[48px] cursor-pointer' src="/assets/shared/logo.svg" alt="" />
+    <div className='w-full h-[88px] lg:h-[96px] flex justify-between lg:justify-end items-center font-barlowCondensed tracking-widest relative'>
+        <div className='h-[px] w-[187.5px] bg-black lg:absolute lg:left-0 z-40 lg:h-[48px] lg:min-w-[160px] pl-[1.5em] lg:pl-[2.5em] lg:justify-start flex'>
+            <Link><img className='h-[40px] w-[40px] lg:w-[48px] lg:h-[48px] cursor-pointer' src="/assets/shared/logo.svg" alt="" /></Link>
         </div>
-        <div className=' h-[40px] w-[187.5px] lg:w-[640px] lg:h-[96px] flex justify-end items-center lg:absolute right-0 top-0 lg:pr-0 pr-[1.5em] '>
+        <div className=' h-[40px] w-[187.5px] lg:h-[96px] flex justify-end items-center lg:absolute right-0 top-0 lg:pr-0 pr-[1.5em] '>
             <Link><img className='h-[21px] w-[24px] lg:hidden' src="/assets/shared/icon-hamburger.svg" alt="" /></Link>
         </div>
-        <ul className='h-full w-full flex-row lg:backdrop-blur-xl lg:bg-white/5 justify-end items-center pr-[3em] hidden gap-[3em] text-[1rem] lg:flex relative z-50 text-white uppercase'>
+        <ul className='h-full lg:w-[640px] flex-row lg:backdrop-blur-xl lg:bg-white/5 justify-end items-center pr-[3em] hidden gap-[3em] text-[1rem] lg:flex relative z-10 text-white uppercase'>
           {router && router.map(route => {
             return (
               <li key={route.id} className={`h-full items-center flex-col justify-center relative z-20 border-b-4 flex gap-3 hover:border-opacity-50 ease-in-out duration-700 hover:border-b-white ${hoverUnderlineColor === route.id ? 'border-b-white' : 'border-b-transparent'}`}>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,22 +8,22 @@ const Home = () => {
 
   return (
     <div className='flex flex-col bg-fixed items-center bg-parallax h-screen bg-no-repeat bg-cover overflow-hidden w-full break-all'>
-        <div className='overflow-scroll no-scrollbar w-full out'>
-          <Header />
-          <div className='p-[1.5em] lg:pb-[1.5em] lg:pl-[1.5em] lg:pr-[1.5em] lg:pt-[8em] text-white flex flex-col'>
-            <div className='flex flex-col gap-[1em] text-center'>
+        <div className='overflow-scroll no-scrollbar w-full'>
+          <Header /> 
+          <div className='p-[1.5em] lg:pb-[1.5em] lg:pl-[1.5em] lg:pr-[1.5em] lg:pt-[8em] lg:gap-[80px] text-white flex flex-col '>
+            <div className='flex flex-col gap-[1em] text-center items-center'>
                 {/* check for correct tracking dimension (h1) */}
-              <h1 className='text-[1rem] lg:text-[1.25em] flex flex-col justify-center items-center text-lightBlue font-barlowCondensed uppercase tracking-[0.125em]'>So, you want to travel to <br />
-                <span className='text-[5rem] lg:text-[5em] text-white uppercase font-normal font-bellefair'>space</span>
-              </h1>
+              <h1 className='text-[1rem] lg:text-[1.75rem] flex flex-col justify-center items-center text-lightBlue font-barlowCondensed uppercase break-all tracking-[0.125em] '>So, you want to travel to</h1>
+              <h1 className='text-[5rem] lg:text-[9rem] text-white uppercase font-bellefair'>space</h1>
               {/* make sure the break-all is not making the para undreadable */}
-              <p className='text-lightBlue font-barlow leading-[180%] text-[0.9375em]'>Let’s face it; if you want to go to space, you might as well genuinely go to
+              <p className='text-lightBlue font-barlow leading-[180%] text-[0.9375em] lg:text-[1rem] lg:w-[510px]'>Let’s face it; if you want to go to space, you might as well genuinely go to
                 outer space and not hover kind of on the edge of it. Well sit back, and relax
                 because we’ll give you a truly out of this world experience!</p>
             </div>
-            <div className=' h-[382px]  outline-4 w-full flex justify-center items-center relative z-30'>
+            <div className=' h-[382px] lg:h-[272px] outline-4 w-full flex justify-center items-center relative z-30'>
                 <button 
                 // onClick={() => handleHoverOver()}
+
                 onMouseEnter={() => setHover("white")} 
                 onMouseLeave={() => setHover("")}
                 onTouchStart={() => setHover("white")} 
@@ -31,7 +31,7 @@ const Home = () => {
 
                 className='z-50 font-bellefair relative cursor-pointer uppercase text-[1.125rem] lg:text-[2rem] text-veryDarkNavy w-[144px] lg:w-[272px] lg:min-h-[272px] min-h-[144px] rounded-full flex justify-center items-center bg-white select-none'>Explore</button>
                 {/* look for the right dimensions */}
-                <div className={`w-[420px] h-[420px] flex justify-center items-center rounded-full bg-${hover} opacity-[10%] absolute ease-in-out duration-700 z-40`}></div>
+                <div className={`w-[480px] h-[480px] flex justify-center items-center rounded-full bg-${hover} opacity-[10%] absolute ease-in-out duration-700 z-40`}></div>
             </div>
           </div>
         </div>
