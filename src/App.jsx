82d6de34ab038dd7774c88e7847744aa1120a649
code-looney <danielvbofficieel@gmail.com>
2024-07-30 14:45:48 +0200

    created routes for all nav links

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
+import Destination from './Destination'
+import Crew from './Crew'
+import Technology from './Technology'
 
 function App() {
 
@@ -9,7 +12,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path={`/`} element={<Home />} />
-        {/* <Route path={`/destination`} element={<Destination />} />  */}
+        <Route path={`/destination`} element={<Destination />} />
+        <Route path={`/crew`} element={<Crew />} />
+        <Route path={`/technology`} element={<Technology />} />
       </Routes>
     </BrowserRouter>
   )
