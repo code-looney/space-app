a38b0a0a1ae95ae649a0eee3b4b29c61a7572a4f
code-looney <danielvbofficieel@gmail.com>
2024-07-31 22:37:36 +0200

    testing with  bg

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
 
                 className='z-50 font-bellefair relative cursor-pointer uppercase text-[1.125rem] lg:text-[2rem] 
                 text-red-600 w-[144px] lg:w-[272px] lg:min-h-[272px] min-h-[144px] rounded-full 
-                flex justify-center items-center  select-none'>{context.mainButtonText}</button>
+                flex justify-center items-center bg-white select-none'>{context.mainButtonText}</button>
                 {/* look for the right dimensions */}
                 <div className={`w-[480px] h-[480px] flex justify-center items-center rounded-full bg-${context.hover} opacity-[10%] absolute ease-in-out duration-700 z-40`}></div>
             </div>
