469005ae801865b3f675abeb6cef86cc272ea4ca
code-looney <danielvbofficieel@gmail.com>
2024-08-04 20:20:16 +0200

    added hr to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,17 +7,20 @@ const Header = () => {
 
   return (
     <div className='w-full h-[88px] lg:h-[96px] flex justify-between lg:justify-end items-center font-barlowCondensed tracking-widest relative'>
-        <div className='xl:w-[560p] gap-[64px] items-center lg:absolute lg:left-0 z-40 lg:h-[48px] lg:min-w-[160px] pl-[1.5em] lg:pl-[2.5em] lg:justify-start flex'>
-            <Link><img className='h-[40px] w-[40px] lg:w-[48px] lg:h-[48px] cursor-pointer' src="/assets/shared/logo.svg" alt="" /></Link>
-            <hr className='w-[700px] hidden xl:block bg-black' />
+        <div className='xl:w-full relative gap-[64px] items-center lg:absolute lg:left-0 z-40 xl:z-0 lg:h-[48px] lg:min-w-[160px] pl-[1.5em] lg:pl-[2.5em] lg:justify-start flex'>
+            <Link><img className='h-[40px] w-[40px] lg:w-[48px] lg:h-[48px] cursor-pointer' src="/assets/shared/logo.svg" alt="logo" /></Link>
+            <hr className='w-full translate-x-32 hidden absolute xl:block' />
         </div>
         <div className=' h-[40px] w-[187.5px] lg:h-[96px] flex justify-end items-center lg:absolute right-0 top-0 lg:pr-0 pr-[1.5em] '>
-            <Link onClick={() => context.handleToggleMobileMenu()}><img className='lg:hidden' src="/assets/shared/icon-hamburger.svg" alt="hamburger icon " /></Link>
+            <Link onClick={() => context.handleToggleMobileMenu()}><img className='lg:hidden' src="/assets/shared/icon-hamburger.svg" alt="hamburger icon" /></Link>
         </div>
-        <ul className='h-full lg:w-[640px] flex-row lg:backdrop-blur-xl lg:bg-white/5 justify-end items-center pr-[3em] hidden gap-[3em] text-[1rem] lg:flex relative z-10 text-white uppercase'>
+        <ul className='h-full lg:w-[640px] flex-row lg:backdrop-blur-xl lg:bg-white/5 justify-end z-10 xl:z-0 items-center pr-[3em] hidden gap-[3em] text-[1rem] lg:flex relative text-white uppercase'>
+        <li className=' w-full xl:block hidden'>
+        <hr className='w-full text-[#979797] hidden xl:block' />
+        </li>
           {context.router && context.router.map(route => {
             return (
-              <li key={route.id} className={`h-full items-center flex-col justify-center relative z-20 border-b-4 flex gap-3 ${context.hoverUnderlineColor === route.id ? null : "hover:border-opacity-50 "} ease-in-out duration-700 hover:border-b-white 
+              <li key={route.id} className={`h-full items-center flex-col justify-center relative z-20 xl:z-0 border-b-4 flex gap-3 ${context.hoverUnderlineColor === route.id ? null : "hover:border-opacity-50 "} ease-in-out duration-700 hover:border-b-white 
               ${context.hoverUnderlineColor === route.id ? 'border-b-white' : 'border-b-transparent'}`}>
               <NavLink to={`/`}
                   onClick={() => context.handleNavLinkHover(route.id)}
