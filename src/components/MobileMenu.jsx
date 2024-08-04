d7c7b4e3fe0224e352f968b7febc471703abb658
code-looney <danielvbofficieel@gmail.com>
2024-08-04 05:33:07 +0200

    added add div and enc the Transition comp

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -8,37 +8,38 @@ const MobileMenu = () => {
     console.log(context.toggleMobileMenu)
 
     return (
-        <Transition
-          show={context.toggleMobileMenu}
-          enter="transition-transform duration-700 ease-in-out"
-          enterFrom="transform translate-x-full opacity-0"
-          enterTo="transform translate-x-0 opacity-100"
-          leave="transition-transform duration-700 ease-in-out"
-          leaveFrom="transform translate-x-0 opacity-100"
-          leaveTo="transform translate-x-full opacity-0"
-          >
-            <div className={`flex-col w-[254px] h-full right-0 top-0 absolute no-scrollbar 
-              ${context.toggleMobileMenu === false ? `transition transform duration-700 ease-in-out translate-x-0 opacity-100` : "" }
-               text-white backdrop-blur-xl z-40 bg-white/5`}>
-                <ul className='h-full w-full flex flex-col gap-[2em]'>
-                    <li className='h-[88px] lg:h-[96px] flex justify-end items-center pr-[1.5em]'>
-                        <button aria-expanded={context.toggleMobileMenu}  onClick={context.handleToggleMobileMenu}>
-                            <img className='w-[24px]' src={`/assets/shared/icon-close.svg`} alt="menu icon" />
-                        </button>
-                    </li>
-                    {context.router && context.router.map(route => (
-                        <li key={route.id} className={`pl-[2em] flex uppercase tracking-[0.125em] border-r-4 gap-3 hover:border-opacity-50 ease-in-out duration-700 hover:border-r-white 
-                            ${context.hoverUnderlineColor === route.id ? 'border-r-white' : 'border-r-transparent'}`}>
-                            <NavLink onClick={() => context.handleNavLinkHover(route.id)} 
-                                className="flex gap-3 justify-self-center text-[16px] font-barlowCondensed">
-                                <span className='font-bold'>{route.order}</span>
-                                {route.page}
-                            </NavLink>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </Transition>
+        <div className='absolute flex-col w-[254px] h-full right-0 top-0 no-scrollbar text-white backdrop-blur-xl z-40 bg-white/5'>
+          <Transition
+            show={context.toggleMobileMenu}
+            enter="transition-transform duration-700 ease-in-out"
+            enterFrom="transform translate-x-full opacity-0"
+            enterTo="transform translate-x-0 opacity-100"
+            leave="transition-transform duration-700 ease-in-out"
+            leaveFrom="transform translate-x-0 opacity-100"
+            leaveTo="transform translate-x-full opacity-0"
+            >
+              <div className={`
+                ${context.toggleMobileMenu === false ? `transition transform duration-700 ease-in-out translate-x-0 opacity-100` : "" }`}>
+                  <ul className='h-full w-full flex flex-col gap-[2em]'>
+                      <li className='h-[88px] lg:h-[96px] flex justify-end items-center pr-[1.5em]'>
+                          <button aria-expanded={context.toggleMobileMenu}  onClick={context.handleToggleMobileMenu}>
+                              <img className='w-[24px]' src={`/assets/shared/icon-close.svg`} alt="menu icon" />
+                          </button>
+                      </li>
+                      {context.router && context.router.map(route => (
+                          <li key={route.id} className={`pl-[2em] flex uppercase tracking-[0.125em] border-r-4 gap-3 hover:border-opacity-50 ease-in-out duration-700 hover:border-r-white
+                              ${context.hoverUnderlineColor === route.id ? 'border-r-white' : 'border-r-transparent'}`}>
+                              <NavLink onClick={() => context.handleNavLinkHover(route.id)}
+                                  className="flex gap-3 justify-self-center text-[16px] font-barlowCondensed">
+                                  <span className='font-bold'>{route.order}</span>
+                                  {route.page}
+                              </NavLink>
+                          </li>
+                      ))}
+                  </ul>
+              </div>
+          </Transition>
+        </div>
     );
 }
 
